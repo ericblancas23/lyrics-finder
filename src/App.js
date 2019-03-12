7aafe29c6be03abba6bd6eaa215b4f76fb19950b
EricB <ericblancas23@gmail.com>
2019-03-12 11:00:00 -0700

    first half

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+import { Provider } from './context';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+      <Router>
+      <React.Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Index}></Route>
+            <Route exact path="/" component={Index}></Route>
+          </Switch>
+        </div>
+      </React.Fragment>
+      </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
