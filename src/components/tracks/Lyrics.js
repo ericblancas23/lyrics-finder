13523fd9748fc8f1f2b25ac5b0cc58afd2779db5
EricB <ericblancas23@gmail.com>
2019-03-19 10:38:51 -0700

    completed forum

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -48,11 +48,32 @@ export default class Lyrics extends Component {
             Goback
           </Link>
           <div className="card">
-            <div className="card-header">
+            <h5 className="card-header">
               {track.track_name} by{" "}
               <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <div className="card-text">{lyrics.lyrics_body}</div>
             </div>
           </div>
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong>Album ID</strong>: {track.album_id}
+            </li>
+            <li className="list-group-item">
+              <strong>Song Genre</strong>:{" "}
+              {
+                track.primary_genres.music_genre_list[0].music_genre
+                  .music_genre_name
+              }
+            </li>
+            <li className="list-group-item">
+              <strong>Explicit</strong>: {track.explicit === 0 ? "No" : "Yes"}
+            </li>
+            <li className="list-group-item">
+              <strong>Release Date</strong>: {track.first_release_date}
+            </li>
+          </ul>
         </React.Fragment>
       );
     }
