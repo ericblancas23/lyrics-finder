e929c71be9f0497ccf95c26c1507951e2b9240ab
EricB <ericblancas23@gmail.com>
2019-03-26 15:34:51 -0700

    added payload and search query

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -7,8 +7,9 @@ export default class Search extends Component {
     trackTitle: ""
   };
 
-  findTrack = e => {
+  findTrack = (dispatch,e) => {
     e.preventDefault();
+
 
     axios
       .get(
@@ -19,7 +20,10 @@ export default class Search extends Component {
         }`
       )
       .then(res => {
-        console.log(res.data);
+       dispatch({
+           type: 'SEARCH_TRACKS',
+           payload: res.data.message.body.track_list
+       })
       })
       .catch(err => console.log(err));
   };
@@ -31,13 +35,14 @@ export default class Search extends Component {
     return (
       <Consumer>
         {value => {
+          const { dispatch } = value;
           return (
             <div className="card card-body mb-4 p-4">
               <h1 className="display-4 text-center">
                 <i className="fas fa-music" /> Search for a song
               </h1>
               <p className="lead text-center">Get the lyrics for any song</p>
-              <form onSubmit={this.findTrack}>
+              <form onSubmit={this.findTrack.bind(this, dispatch)}>
                 <div className="form-group">
                   <input
                     type="text"
